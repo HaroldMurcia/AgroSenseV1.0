264750a02f6044743c2ef2544099add6873ef249
Harold F. Murcia <harold.murcia@unibague.edu.co>
2017-03-22 16:44:34 -0500

    This Version includes smtp messages

--- CollectorNode/Collector.py
+++ CollectorNode/Collector.py
@@ -58,82 +58,79 @@ def ThingSpeak(KEY,d1,d2,d3,d4,d5,d6,d7,d8):
             print "Conection Failed"
 
 def InternetTest():
-	try:
-        	_ = requests.get(urlTest, timeout=timeoutTest)
-        	print("Internet ok")
-		InternetFail=0
-	except requests.ConnectionError:
-        	print("No internet")
-		print("-------------------------")
-		InternetFail=InternetFail+1
-		if(IntenernetFail>120):
-			ErrorMode=2
-			print "Reboot"
-			time.sleep(1)
-			os.system("sudo reboot")
-
+    try:
+        _ = requests.get(urlTest, timeout=timeoutTest)
+        print("Internet ok")
+        InternetFail=0
+    except requests.ConnectionError:
+        print("No internet")
+        print("-------------------------")
+        InternetFail=InternetFail+1
+        if(IntenernetFail>120):
+            ErrorMode=2
+            InternetFail=0
+            print "Reboot"
+            time.sleep(1)
+            os.system("sudo reboot")
+#-------------------------------------------------------------------- MAIN
 if __name__ == "__main__":
-        print('Iniciando... ')
-        fileText=time.strftime("%d-%m-%y")+'-'+time.strftime("%I-%M-%S")+".txt"
-	print("Fichero: "+fileText)
-	f = open(fileText,'w')
+    print('Iniciando... ')
+    fileText=time.strftime("%d-%m-%y")+'-'+time.strftime("%I-%M-%S")+".txt"
+    print("Fichero: "+fileText)
+    f = open(fileText,'w')
+    f.close()
+    InternetTest()
+    if(InternetFail==0):
+        sendEmail('harold.murcia@unibague.edu.co','System State','System OK','')
+    while True:
+        f = open(fileText,"a") #opens file with name of "test.txt"
+        Tic=time.time()
+        try:
+            dataXbee=Xbee.readline()
+            dataFile=dataXbee
+            dataFile=dataFile.strip()
+            Times='\t'+time.strftime("%y")+'\t'+time.strftime("%m")+'\t'+time.strftime("%d")+'\t'+time.strftime("%I")+'\t'+time.strftime("%M")+'\t'+time.strftime("%S")+'\r\n'
+            dataFile=dataFile+Times
+            f.write(dataFile)
+            if(dataXbee==''):
+                print("Xbee Fail")
+                XbeeFail=XbeeFail+1
+                if(XbeeFail>10):
+                    XbeeFail=0
+                    sendEmail('harold.murcia@unibague.edu.co','Fail','Xbee Fail','')
+                    ErrorMode=1
+                    print "Reboot"
+                    time.sleep(1)
+                    #os.system("sudo reboot")
+            else:
+                print "Xbee Ok"
+                XbeeFail=0
+        except KeyboardInterrupt:
+            break
+        data=dataXbee.split('\t')
+        N=len(data)
+        print('N: '+str(N))
+        print ('Data: '+str(data))
+        try:
+            if(N>19):
+                ThingSpeak(THINGSPEAKKEY,data[17],data[9],data[10],data[11],data[12],data[13],data[19],data[16])
+        except:
+            print('Error uploading Data')
+        Toc=time.strftime("%I-%M-%S")
+        Toc=Toc.split('-')
+        Hour=float(Toc[0])
+        Minutes=float(Toc[1])
+        if(Hour!=0):
+            DataReport=0
+        if(Hour==0 and Minutes>0 and Minutes<30 and DataReport==0):
+            DataReport=1
+            f.close()
+            sendEmail('harold.murcia@unibague.edu.co','Report','This is the data report',fileText)
+            fileText=time.strftime("%d-%m-%y")+'-'+time.strftime("%I-%M-%S")+".txt"
+            print("Fichero: "+fileText)
+            f = open(fileText,'w')
+            f.close()
+        Toc=time.time()
+        dT=Toc-Tic
         f.close()
-        while True:
-                f = open(fileText,"a") #opens file with name of "test.txt"
-                Tic=time.time()
-		try:
-			dataXbee=Xbee.readline()
-			dataFile=dataXbee
-			dataFile=dataFile.strip()
-			Times='\t'+time.strftime("%y")+'\t'+time.strftime("%m")+'\t'+time.strftime("%d")+'\t'+time.strftime("%I")+'\t'+time.strftime("%M")+'\t'+time.strftime("%S")+'\r\n'
-			dataFile=dataFile+Times
-			f.write(dataFile)
-			if(dataXbee==''):
-				print("Xbee Fail")
-				XbeeFail=XbeeFail+1
-				if(XbeeFail>10):
-					XbeeFail=0
-					if(InternetFail==0):
-						sendEmail('harold.murcia@unibague.edu.co','Fail','Xbee Fail','')
-					ErrorMode=1
-					print "Reboot"
-					time.sleep(1)
-					#os.system("sudo reboot")
-			else:
-				print "Xbee Ok"
-				XbeeFail=0
-		except KeyboardInterrupt:
-			break
-		InternetTest()
-		print XbeeFail
-		data=dataXbee.split('\t')
-		N=len(data)
-		print('N: '+str(N))
-                print ('Data: '+str(data))
-                try:
-               		if(N>19):
-                		ThingSpeak(THINGSPEAKKEY,data[17],data[9],data[10],data[11],data[12],data[13],data[19],data[16])
-                except:
-                	print('Error uploading Data')
-                Toc=time.strftime("%I-%M-%S")
-		Toc=Toc.split('-')
-		Hour=float(Toc[0])
-		Minutes=float(Toc[1])
-		if(Hour!=0):
-			DataReport=0
-		if(Hour==0 and Minutes>0 and Minutes<30 and DataReport==0):
-			DataReport=1
-			f.close()
-			sendEmail('harold.murcia@unibague.edu.co','Report','This is the data report',fileText)
-			fileText=time.strftime("%d-%m-%y")+'-'+time.strftime("%I-%M-%S")+".txt"
-        		print("Fichero: "+fileText)
-        		f = open(fileText,'w')
-        		f.close()
-		# Sample Time
-                Toc=time.time()
-                dT=Toc-Tic
-                f.close()
-                print('Dt: '+ str(dT))
-                #while(Ts>dT):
-                #    Toc=time.time()
-                #    dT=Toc-Tic
+        print('Dt: '+ str(dT))
